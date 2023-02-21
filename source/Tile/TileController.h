795d8e679163791c47d7af0649cc0c0f941db141
Soon Jae <ryupark1321@gmail.com>
2023-02-21 17:00:43 -0500

    Added Tile MVC

---
+++ source/Tile/TileController.h
@@ -0,0 +1,115 @@
+//
+//  TileController.h
+//  Sunk Cost  
+//
+//  This module provides the TileController class.
+//
+//  Author of Referenced File: Gonzalo Gonzalez
+//  Version: 2/21/23.
+//
+#ifndef _TILE_CONTROLLER_H__
+#define _TILE_CONTROLLER_H__
+
+// These do not need to be in angle brackets
+#include "TileModel.h"
+#include "TileView.h"
+
+/**
+ * A class communicating between the model and the view. It only
+ * controls a single tile.
+ */
+class TileController {
+    
+#pragma mark Internal References
+private:
+    /** Model reference */
+    std::unique_ptr<TileModel> _model;
+    /** View reference */
+    std::unique_ptr<TileView> _view;
+    
+#pragma mark Main Methods
+public:
+    /**
+     * Creates a controller for the model and view.
+     *
+     * @param position  The bottom left corner of the tile
+     * @param size      The width and height of a tile
+     * @param color     The tile color tint
+     * @param color     The tile color
+     */
+    TileController(Vec2 position, Size size, Color4 color, bool traversable, const std::shared_ptr<Texture> &texture) {
+        // TODO: Implement me
+        _model = std::make_unique<TileModel>(position, size, color, traversable); 
+        _view = std::make_unique<TileView>(position, size, color, texture); 
+    }
+    
+#pragma mark Update Methods
+public:
+    /**
+     *  Updates the model and view with position of this tile.
+     *
+     *  @param position  The bottom left corner of the tile
+     */
+    void updatePosition(Vec2 position) {
+        // TODO: Implement me
+        _model->setPosition(position);
+        _view->setPosition(position);
+    }
+    
+    /**
+     *  Updates the model and view with the size of this tile.
+     *
+     *  @param size  The bottom left corner of the tile
+     */
+    void updateSize(Size size) {
+        // TODO: Implement me
+        _model->setSize(size);
+        _view->setSize(size);
+    }
+    
+    /**
+     *  Updates the model and view with the color of this tile.
+     *
+     *  @param color The tile color
+     */
+    void updateColor(Color4 color) {
+        // TODO: Implement me
+        _model->setColor(color);
+        _view->setColor(color);
+    }
+
+    /**
+     *  Updates the model and view with the color of this tile.
+     *
+     *  @param traverse The tile traversability
+     */
+    void updateTraversable(bool traverse) {
+        // TODO: Implement me
+        _model->setTraversable(traverse);
+    }
+    
+#pragma mark Scene Methods
+public:
+    /**
+     * Adds the view as a child to the given `node`.
+     *
+     * @param node The scenenode to add the view to
+     */
+    void addChildTo(std::shared_ptr<scene2::SceneNode> node) {
+        // TODO: Implement me
+        _view->addChildTo(node);
+    }
+    
+    /**
+     * Removes the view child from the given `node`.
+     *
+     * @param node The scenenode to remove the view from
+     */
+    void removeChildFrom(std::shared_ptr<scene2::SceneNode> node) {
+        // TODO: Implement me
+        _view->removeChildFrom(node);
+    }
+};
+
+
+#endif /* _TILE_CONTROLLER_H__ */
---
+++ source/Tile/TileModel.h
@@ -0,0 +1,103 @@
+//
+//  TileModel.h
+//  Sunk Cost
+//
+//  This module provides the TileModel class.
+//
+//  Author of Referenced File: Gonzalo Gonzalez
+//  Version: 2/21/23.
+//
+#ifndef _TILE_MODEL_H__
+#define _TILE_MODEL_H__
+
+#include <cugl/cugl.h>
+using namespace cugl;
+
+/**
+ * A class representing a single tile
+ */
+class TileModel {
+    
+#pragma mark State
+private:
+    /** Bottom left corner of the tile */
+    Vec2 _position;
+    /** Width and height of a single tile */
+    Size _size;
+    /** Tile color */
+    Color4 _color;
+    /** Whether this tile is traversable or not */
+    bool _traversable;
+    
+public:
+    /** A public accessible, read-only version of the color */
+    const Color4& color;
+    /** A public accessible, read-only version of _traversable */
+    const bool& _traversable;
+    
+    
+#pragma mark Main Functions
+public:
+    /**
+     * Creates the model state.
+     *
+     * @param position     The bottom left corner of the tile
+     * @param size         The width and height of a tile
+     * @param color        The tile color
+     * @param traversable  The tile traversability
+     */
+    TileModel(Vec2 position, Size size, Color4 color, bool traversable): 
+    color(_color), 
+    traversable(_traversable) {
+        // TODO: Implement me
+        setPosition(position);
+        setSize(size);
+        setColor(color);
+        setTraversable(traversable);
+    }
+
+#pragma mark Setters
+public:
+    /**
+     *  Sets the position of the bottom left corner of the tile.
+     *
+     *  @param position Bottom left corner of tile
+     */
+    void setPosition(Vec2 position) {
+        // TODO: Implement me
+        _position = position;
+    }
+    
+    /**
+     *  Sets the size of the tile.
+     *
+     *  @param size Width and height of a single tile
+     */
+    void setSize(Size size) {
+        // TODO: Implement me
+        _size = size;
+    }
+    
+    /**
+     *  Sets the color of the tile.
+     *
+     *  @param color The color of the tile
+     */
+    void setColor(Color4 color) {
+        // TODO: Implement me
+        _color = color;
+    }
+
+    /**
+     *  Sets the traversability of the tile.
+     *
+     *  @param traversable The traversability of the tile
+     */
+    void setTraversable(bool traversable) {
+        // TODO: Implement me
+        _traversable = traversable;
+    }
+    
+};
+
+#endif /* _TILE_MODEL_H__ */

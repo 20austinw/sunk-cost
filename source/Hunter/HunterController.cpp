4152baf4ea4ed86539c658369203a7bf8973a287
Chloe Chu <cac466@cornell.edu>
2023-02-23 00:31:34 -0500

    Please rebase

--- source/Hunter/HunterController.cpp
+++ source/Hunter/HunterController.cpp
@@ -1,8 +1,34 @@
 //
 //  HunterController.cpp
-//  Sunk
+//  Sunk Cost
 //
-//  Created by chloe on 2/22/23.
+//  Created by Chloe Chu
+//  Version: 2/22/23.
 //
 
-#include <stdio.h>
+#include "HunterController.h"
+
+#pragma mark Main Functions
+/**
+ * TODO: Implement Me
+ * The constructor should set up the model, view, and camera controller
+ */
+HunterController::HunterController() {}
+
+/**
+ * TODO: Implement Me
+ * This function should
+ * (1) get the view from CameraController
+ * (2) attach the viewport to the scene
+ */
+void HunterController::render() {}
+
+/**
+ * TODO: Implement Me
+ * This function should (for gameplay prototype)
+ * (1) detect camera change
+ * (2) modify camera to reflect the change
+ */
+void HunterController::update() {}
+
+
--- source/Hunter/HunterController.h
+++ source/Hunter/HunterController.h
@@ -1,12 +1,91 @@
 //
 //  HunterController.h
-//  Sunk
+//  Sunk Cost
 //
-//  Created by chloe on 2/22/23.
+//  Created by Chloe Chu
+//  Version: 2/22/2023
 //
 
-#ifndef HunterController_h
-#define HunterController_h
+#ifndef _HUNTER_CONTROLLER_H
+#define _HUNTER_CONTROLLER_H
 
+#include <cugl/cugl.h>
+#include "HunterModel.h"
+#include "HunterView.h"
+#include "../Camera/CameraController.h"
 
-#endif /* HunterController_h */
+using namespace cugl;
+
+class HunterController {
+#pragma mark Internal References
+private:
+    /** Model reference */
+    std::unique_ptr<HunterModel> _model;
+    /** View reference */
+    std::unique_ptr<HunterView> _view;
+    
+#pragma mark External References
+private:
+    /** Camera reference */
+    std::unique_ptr<CameraController> _camera;
+    
+#pragma mark Constants
+private:
+    /** The preset cooltime for hiding */
+    float _hideCool;
+    
+#pragma mark Main Functions
+public:
+    /**
+     * TODO: Implement Me
+     * The constructor should set up the model, view, and camera controller
+     */
+    HunterController();
+    
+    /**
+     * Gets the viewpoint for the hunter's camera
+     *
+     * @return rectangle that should be drawn
+     */
+    const Rect getCameraView() {
+        return _camera->getViewPort();
+    }
+    
+    /**
+     * TODO: Implement Me
+     * This function should
+     * (1) get the view from CameraController
+     * (2) attach the viewport to the scene
+     */
+    void render();
+    
+    /**
+     * TODO: Implement Me
+     * This function should (for gameplay prototype)
+     * (1) detect camera change
+     * (2) modify camera to reflect the change
+     */
+    void update();
+    
+#pragma mark Setters
+public:
+    /**
+     * Updates  position of the hunter
+     *
+     * @param position  hunter's new position
+     */
+    void updatePosition(cugl::Vec2 position) {
+        _model->setPosition(position);
+        _view->setPosition(position);
+    }
+    
+    /**
+     * Updates remaining hide cooldown time for this hunter
+     *
+     * @param hideCool the new remaining cooltime
+     */
+    void updateHideCooldown(float hideCool) {
+        _model->setHideCooldown(hideCool);
+    }
+};
+#endif /* _HUNTER_CONTROLLER_H__ */

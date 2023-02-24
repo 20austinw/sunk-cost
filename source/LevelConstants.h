f3ee9b596e148d2d48d00c9448d2f22226a5f910
Soon Jae <ryupark1321@gmail.com>
2023-02-23 19:14:30 -0500

    Added LevelModel

---
+++ source/LevelConstants.h
@@ -0,0 +1,52 @@
+//
+// Created by ryupark1321 on 2/23/23.
+//
+
+#ifndef _LEVEL_CONSTANTS_H
+#define _LEVEL_CONSTANTS_H
+
+//
+//  JSLevelConstants.h
+//  JSONDemo
+//
+//  This file contains all of the details that define the JSON structure of a LevelModel.
+//  Created by Walker White on 1/20/16.
+//
+//  Author of Referenced Files: Walker White
+//  Version: 2/23/23
+//
+#ifndef _LEVEL_CONSTANTS_H__
+#define _LEVEL_CONSTANTS_H__
+
+/** The global fields of the level model */
+#define WIDTH_FIELD         "width"
+#define HEIGHT_FIELD        "height"
+#define TILE_FIELD          "tile"
+#define PORTRAIT_FIELD      "portrait"
+#define PLAYER_FIELD        "player"
+
+/** The physics fields for each object */
+#define POSITION_FIELD      "pos"
+#define SIZE_FIELD          "size"
+#define BODYTYPE_FIELD      "bodytype"
+#define DENSITY_FIELD       "density"
+#define ROTATION_FIELD      "rotation"
+#define STATIC_VALUE        "static"
+
+/** The drawing fields for each object */
+#define TEXTURE_FIELD       "texture"
+
+/** The wall specific fields */
+#define VERTICES_FIELD      "polygon"
+#define BOUNDARY_FIELD      "boundary"
+
+/** The source for our level file */
+#define LEVEL_ONE_FILE      "json/gppbasic_level.json"
+/** The key for our loaded level */
+#define LEVEL_ONE_KEY       "basic_level"
+
+
+#endif /* defined(__TD_LEVEL_CONSTANTS_H__) */
+
+
+#endif //_LEVEL_CONSTANTS_H
---
+++ source/LevelModel.h
@@ -0,0 +1,180 @@
+//
+// Created by ryupark1321 on 2/23/23.
+//
+
+#ifndef _LEVEL_MODEL_H
+#define _LEVEL_MODEL_H
+
+#include <cugl/cugl.h>
+#include <vector>
+#include <cugl/assets/CUAsset.h>
+#include <cugl/io/CUJsonReader.h>
+
+using namespace cugl;
+
+#pragma mark -
+#pragma mark Level Model
+class LevelModel : Asset {
+  /** The bounds of this level in physics coordinates */
+  Rect _bounds;
+
+  /** Vector of textures for tiles */
+  std::vector<std::vector<std::string>> _tiles;
+
+  /** Dimenisons of this tilemap */
+  Size _dimensions;
+
+  /** Tile Size of this tilemap */
+  Size _tileSize;
+
+  /** Position of the default camera */
+  Vec2 _defaultcam;
+
+  /** Vector of positions of portraits */
+  std::vector<Vec2> _portraits;
+
+  /** Position data of player */
+  Vec2 _player;
+
+  /** Reference to vector of tiles */
+  Size _mapSize;
+
+  /** The AssetManager for the game mode */
+  std::shared_ptr<cugl::AssetManager> _assets;
+
+public:
+#pragma mark Static Constructors
+  /**
+   * Creates a new game level with no source file.
+   *
+   * The source file can be set at any time via the setFile() method. This method
+   * does NOT load the asset.  You must call the load() method to do that.
+   *
+   * @return  an autoreleased level file
+   */
+  static std::shared_ptr<LevelModel> alloc() {
+    std::shared_ptr<LevelModel> result = std::make_shared<LevelModel>();
+    return (result->init("") ? result : nullptr);
+  }
+
+  /**
+   * Creates a new game level with the given source file.
+   *
+   * This method does NOT load the level. You must call the load() method to do that.
+   * This method returns false if file does not exist.
+   *
+   * @return  an autoreleased level file
+   */
+  static std::shared_ptr<LevelModel> alloc(std::string file) {
+    std::shared_ptr<LevelModel> result = std::make_shared<LevelModel>();
+    return (result->init(file) ? result : nullptr);
+  }
+
+#pragma mark Physics Attributes
+  /**
+   * Returns the bounds of this level in physics coordinates
+   *
+   * @return the bounds of this level in physics coordinates
+   */
+  const Rect& getBounds() const   { return _bounds; }
+
+  /**
+   * Sets the loaded assets for this game level
+   *
+   * @param assets the loaded assets for this game level
+   */
+  void setAssets(const std::shared_ptr<AssetManager>& assets) { _assets = assets;  }
+
+#pragma mark -
+#pragma mark Asset Loading
+  /**
+   * Loads this game level from the source file
+   *
+   * This load method should NEVER access the AssetManager.  Assets are loaded in
+   * parallel, not in sequence.  If an asset (like a game level) has references to
+   * other assets, then these should be connected later, during scene initialization.
+   *
+   * @param file the name of the source file to load from
+   *
+   * @return true if successfully loaded the asset from a file
+   */
+  virtual bool preload(const std::string& file);
+
+
+  /**
+   * Loads this game level from a JsonValue containing all data from a source Json file.
+   *
+   * This load method should NEVER access the AssetManager.  Assets are loaded in
+   * parallel, not in sequence.  If an asset (like a game level) has references to
+   * other assets, then these should be connected later, during scene initialization.
+   *
+   * @param json the json loaded from the source file to use when loading this game level
+   *
+   * @return true if successfully loaded the asset from the input JsonValue
+   */
+  virtual bool preload(const std::shared_ptr<cugl::JsonValue>& json) override;
+
+  /**
+   * Unloads this game level, releasing all sources
+   *
+   * This load method should NEVER access the AssetManager.  Assets are loaded and
+   * unloaded in parallel, not in sequence.  If an asset (like a game level) has
+   * references to other assets, then these should be disconnected earlier.
+   */
+  void unload();
+
+  //#pragma mark -
+  //#pragma mark Initializers
+  /**
+   * Creates a new, empty level.
+   */
+  LevelModel(void);
+
+  /**
+   * Destroys this level, releasing all resources.
+   */
+  virtual ~LevelModel(void);
+
+#pragma mark -
+#pragma mark Individual Loaders
+  /**
+    * Determines which object is currently being parsed,
+    * and then sends the object over
+    * to the respective load function
+    *
+    * @param reader a JSON reader with cursor ready to read the object
+    *
+    * @return true if the object was successfully loaded
+    */
+  bool loadObject(const std::shared_ptr<JsonValue>& json);
+
+  /**
+    * Parses JSON and extracts portrait/default camera positions from it
+    *
+    * @param reader a JSON reader with cursor ready to read the portraitset
+    *
+    * @return true if the object was successfully loaded
+    */
+  bool loadPortraitSetAndDefault(const std::shared_ptr<JsonValue>& json);
+
+  /**
+    * Parses JSON and extracts player positions from it
+    *
+    * @param reader a JSON reader with cursor ready to read the player
+    *
+    * @return true if the object was successfully loaded
+    */
+  bool loadPlayer(const std::shared_ptr<JsonValue>& json);
+
+  /**
+    * Parses JSON and extracts tile positions and textures from it
+    *
+    * @param reader a JSON reader with cursor ready to read the tiles
+    *
+    * @return true if the object was successfully loaded
+    */
+  bool loadTiles(const std::shared_ptr<JsonValue>& json);
+};
+
+
+#endif //_LEVEL_MODEL_H

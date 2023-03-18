a2336753e1734270a09187b43977a1526f40db40
Crystal Jin <xj65@cornell.edu>
2023-03-18 00:27:02 -0400

    Added files for hunter

--- source/Hunter/HunterController.h
+++ source/Hunter/HunterController.h
@@ -9,64 +9,66 @@
 #ifndef _HUNTER_CONTROLLER_H
 #define _HUNTER_CONTROLLER_H
 
-#include "../Camera/CameraController.h"
-#include "../Input/InputController.h"
+#include <cugl/cugl.h>
 #include "HunterModel.h"
 #include "HunterView.h"
-#include <cugl/cugl.h>
+#include "../Camera/CameraController.h"
+#include "../Input/InputController.h"
 
 using namespace cugl;
 
 class HunterController {
 #pragma mark Internal References
-  private:
+private:
     /** Model reference */
-    std::unique_ptr<HunterModel> _model;
+    std::shared_ptr<HunterModel> _model;
     /** View reference */
     std::unique_ptr<HunterView> _view;
-
+    
     /** Position of the hunter */
     cugl::Vec2 _pos;
     /** Velocity of the hunter */
     cugl::Vec2 _vel;
-
+        
     // The following are protected, because they have no accessors
     /** Current angle of the hunter */
     float _ang;
     /** Accumulator variable to turn faster as key is held down */
     float _dAng;
-
+    
 #pragma mark External References
-  private:
+private:
     /** Camera reference */
     std::unique_ptr<CameraController> _camera;
     /** The controller to manage the hunter */
     InputController _input;
     Size _screenSize;
-
+    
 #pragma mark Constants
-  private:
+private:
     /** The preset cooltime for hiding */
     float _hideCool;
-
+    
 #pragma mark Main Functions
-  public:
+public:
+    
     HunterController() {}
-
+    
     /**
      * TODO: Implement Me
      * The constructor should set up the model, view, and camera controller
      */
-    HunterController(const std::shared_ptr<cugl::AssetManager>& assets,
-                     Size screenSize);
-
+    HunterController(const std::shared_ptr<cugl::AssetManager>& assets, Size screenSize);
+    
     /**
      * Gets the viewpoint for the hunter's camera
      *
      * @return rectangle that should be drawn
      */
-    const Rect getCameraView() { return _camera->getViewPort(); }
-
+    const Rect getCameraView() {
+        return _camera->getViewPort();
+    }
+    
     /**
      * TODO: Implement Me
      * This function should
@@ -74,7 +76,7 @@ class HunterController {
      * (2) attach the viewport to the scene
      */
     void render(const std::shared_ptr<cugl::SpriteBatch>& batch);
-
+    
     /**
      * TODO: Implement Me
      * This function should (for gameplay prototype)
@@ -83,13 +85,13 @@ class HunterController {
      */
 
     void setViewFrame(int forward, int right);
-
+    
     void update();
-
+    
     void collisionWithTrap();
-
+    
 #pragma mark Setters
-  public:
+public:
     /**
      * Updates  position of the hunter
      *
@@ -99,7 +101,7 @@ class HunterController {
         _model->setPosition(position);
         _view->setPosition(position);
     }
-
+    
     /**
      * Updates remaining hide cooldown time for this hunter
      *
@@ -108,7 +110,7 @@ class HunterController {
     void updateHideCooldown(float hideCool) {
         _model->setHideCooldown(hideCool);
     }
-
+    
     /**
      * Sets the angle that this hunter is facing.
      *
@@ -118,7 +120,7 @@ class HunterController {
      * @param value the angle of the hunter
      */
     void setAngle(float value) { _ang = value; }
-
+    
     /**
      * Moves the hunter by the specified amount.
      *
@@ -127,23 +129,28 @@ class HunterController {
      */
     void move(float forward, float rightward);
     Vec2 getPosition();
+    
+    b2Body* getHunterBody();
+    
+    void setAsObstacle(std::shared_ptr<cugl::physics2::ObstacleWorld> world);
 #pragma mark View Methods
-    /**
-     * Adds the TilemapView as a child to the given `scene`.
-     *
-     * The TilemapView is the view reference held within the model.
-     *
-     * @param scene The scene to add the view to
-     */
+/**
+ * Adds the TilemapView as a child to the given `scene`.
+ *
+ * The TilemapView is the view reference held within the model.
+ *
+ * @param scene The scene to add the view to
+ */
     void addChildTo(const std::shared_ptr<cugl::Scene2>& scene);
 
-    /**
-     * Removes the TilemapView child from the given `scene`.
-     *
-     * The TilemapView is the view reference held within the model.
-     *
-     * @param scene The scene to remove the view from
-     */
+/**
+ * Removes the TilemapView child from the given `scene`.
+ *
+ * The TilemapView is the view reference held within the model.
+ *
+ * @param scene The scene to remove the view from
+ */
     void removeChildFrom(const std::shared_ptr<cugl::Scene2>& scene);
+    
 };
 #endif /* _HUNTER_CONTROLLER_H__ */

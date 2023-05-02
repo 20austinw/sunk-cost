8aad8a9c272e0d5aa5f743d7045d56c594f1cafa
Zixin Xu <zx35@cornell.edu>
2023-05-02 15:16:12 -0400

    Hunter zoom

--- source/Game/HGameController.cpp
+++ source/Game/HGameController.cpp
@@ -1025,6 +1025,7 @@ void HGameController::initCamera() {
     _scene->getCamera()->setFar(100000);
     _scene->getCamera()->setNear(0);
     _scene->getCamera()->update();
+    std::dynamic_pointer_cast<OrthographicCamera>(_scene->getCamera())->setZoom(2.5);
 }
 
 /**
